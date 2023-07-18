3d4472a75af33c56b5802450a318c6cf82963408
Nicolas RAYNAUD <contact@nicolas-raynaud.fr>
2023-07-18 08:58:54 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/cdan_tp_jpa/CdanTpJpaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.cdan_tp_jpa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CdanTpJpaApplication
+
+fun main(args: Array<String>) {
+    runApplication<CdanTpJpaApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/cdan_tp_jpa/CdanTpJpaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.cdan_tp_jpa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CdanTpJpaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
